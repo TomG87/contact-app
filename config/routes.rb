3fee2f2dbfa2e17ef98402ec1c37bef039ccce78
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-29 12:45:03 -0400

    added jbuilder to Contacts module

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-get "/contact" => "contacts#contact_1"
-  # get "/number_guess", controller: "examples", action: "guess"
-  # get "/test" => "examples#test" 
-  # get "/test2" => "examples#apple"
-  # get "/test3" => "examples#baseball"
+get "/contact" => "contacts#contact"
+get "/all" => "contacts#all"
+get "/number_guess", controller: "examples", action: "guess"
+get "/test" => "examples#test" 
+get "/test2" => "examples#apple"
+get "/test3" => "examples#baseball"
 end
 
