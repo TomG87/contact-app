fae21e239731db8c1bf22adfd4aebf2d5a8b852c
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-29 13:53:17 -0400

    added new contacts from seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# contact = Contact.new(first_name: "Baker", last_name: "O'Neil", email: "Boneil@test.com", phone_number: "230-246-3455").save
+
+contact = Contact.new(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, email: FFaker::Internet.email, phone_number: FFaker::PhoneNumber.phone_number).save
